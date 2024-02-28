0d518f29ef9a7acb8a7a03e651de79c20b12092d
VIVEKTM555 <61287041+VIVEKTM555@users.noreply.github.com>
2024-02-28 12:02:48 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (10.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,13 @@
 class Solution {
-    
-    public int recur(int[] nums, int i,int[] dp){
-        if(i>=nums.length){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        
-        int include=nums[i]+recur(nums,i+2,dp);
-        int exclude=recur(nums,i+1,dp);
-        
-        dp[i]=Math.max(include,exclude);
-        
-        return Math.max(include,exclude);
-    }
     public int rob(int[] nums) {
-        int[] dp=new int[nums.length+1];
-        Arrays.fill(dp,-1);
-        return recur(nums,0,dp);
-        
+        int rob = 0;
+        int norob = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            int newRob = norob + nums[i];
+            int newNoRob = Math.max(norob, rob);
+            rob = newRob;
+            norob = newNoRob;
+        }
+        return Math.max(rob, norob);
     }
 }
